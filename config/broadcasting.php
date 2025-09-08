a82b5b4d02263aa2a840a9d6d800194ff37705b8
rendijsberkis <rendijsberkis7@gmail.com>
2025-09-08 11:53:20 +0300

    web

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -24,15 +24,15 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusherapp.com',
+                'cluster' => 'eu',
+                'host' => env('PUSHER_HOST') ?: 'api-eu.pusherapp.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'cluster' => 'eu',
                 'mt1' => false,
             ],
         ],
